9f980195823a99723a53da1bfac822dc1df0297e
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-06 19:03:43 +0000

    completed 2nd q

--- functions.js
+++ functions.js
@@ -23,4 +23,56 @@ function compare (str1, str2) {
     
 var sortedArray = myArray.sort(compare);
 
-console.log(sortedArray);
+//console.log(sortedArray);
+
+//Create an array of objects 
+// (don’t need to use new here, just regular object literals). 
+//These objects will contain a name and email property. 
+//Then, run some code that will sort your array by the longest name. 
+//Then, run some code that will sort your array by e-mail address in alphabetical order.
+
+var person1 = {name:"Brendan", email:"brendan@email.com"};
+var person2 = {name:"Sean", email:"sean@email.com"};
+var person3 = {name:"Chris", email:"chris@email.com"};
+
+var objArray = [person1, person2, person3];
+
+function potato(person1, person2) {
+  
+    var pOneNameLength = person1.name.length;
+    var pTwoNameLength = person2.name.length;
+  
+        if (pOneNameLength < pTwoNameLength) {
+            return 1;
+        }
+        else if (pOneNameLength > pTwoNameLength) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+var newSortedArray = objArray.sort(potato);
+
+console.log(newSortedArray);
+
+function fancyPotato(person1, person2) {
+  
+    var pOneEmail = person1.email;
+    var pTwoEmail = person2.email;
+  
+        if (pOneEmail < pTwoEmail) {
+            return -1;
+        }
+        else if (pOneEmail > pTwoEmail) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+var secondSortedArray = objArray.sort(fancyPotato);
+
+console.log(secondSortedArray);
